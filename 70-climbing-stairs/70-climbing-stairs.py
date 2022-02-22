f6343e9e8cb3ad695d5ad44b2ae7a77f85424765
Shreya Chandwadkar <62925719+shreyacc2020@users.noreply.github.com>
2022-02-22 02:41:03 -0600

    Time: 57 ms (13.45%), Space: 13.9 MB (85.16%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,10 +2,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         one = 1
         two = 1
-        if n == 2:
-            return 2
-        elif n == 3:
-            return 3
+        # if n == 2:
+        #     return 2
+        # elif n == 3:
+        #     return 3
         for i in range(n-1):
             temp = one
             one = one+two
